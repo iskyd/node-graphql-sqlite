b6a86172b9c4d526a432ca2384b4fcec9b788d08
iskyd <careddumattia@gmail.com>
2021-10-30 13:41:45 +0200

    query and mutuation add

---
+++ README.md
@@ -0,0 +1,10 @@
+# Node + GraphQL + SQLite
+## _RUN_
+
+
+```sh
+npm install
+npm run devStart
+```
+
+Then go to http://localhost:3000
---
+++ server.js
@@ -0,0 +1,199 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const db = require('./database.js')
+
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
+} = require('graphql')
+
+const app = express()
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
+        author: {
+            type: AuthorType,
+            resolve: (book) => {
+                return new Promise((resolve, reject) => {
+                    db.get("SELECT * FROM author WHERE id = ?;", [book.author_id], (err, row) => {
+                        if(err) {
+                            reject(null)
+                        }
+
+                        resolve(row)
+                    })
+                })
+            }
+        }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'This represents an author of a book',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        books: { 
+            type: GraphQLList(BookType),
+            resolve: (author) => {
+                return new Promise((resolve, reject) => {
+                    db.all("SELECT * FROM book WHERE author_id = ?;", [author.id], (err, rows) => {
+                        if(err) {
+                            reject([])
+                        }
+
+                        resolve(rows)
+                    })
+                })
+            }
+        }
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of Books',
+            resolve: () => {
+                return new Promise((resolve, reject) => {
+                    db.all("SELECT * FROM book;", (err, rows) => {
+                        if(err) {
+                            reject([])
+                        }
+
+                        resolve(rows)
+                    })
+                })
+            }
+        },
+        book: {
+            type: BookType,
+            description: 'Single Book',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                return new Promise((resolve, reject) => {
+                    let sql = "SELECT * FROM book WHERE id = ?"
+                    let params = [args.id]
+                    db.get(sql, params, (err, row) => {
+                        if(err) {
+                            reject(null)
+                        }
+
+                        resolve(row)
+                    })
+                })
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            description: 'List of authors',
+            resolve: () => {
+                return new Promise((resolve, reject) => {
+                    db.all("SELECT * FROM author;", (err, rows) => {
+                        if (err) {
+                            reject([]);
+                        }
+                        resolve(rows);
+                    });
+                });
+            }
+        },
+        author: {
+            type: AuthorType,
+            description: 'Single Author',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                return new Promise((resolve, reject) => {
+                    let sql = "SELECT * FROM author WHERE id = ?"
+                    let params = [args.id]
+                    db.get(sql, params, (err, row) => {
+                        if(err) {
+                            reject(null)
+                        }
+
+                        resolve(row)
+                    })
+                })
+            }
+        },
+    })
+})
+
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutuation',
+    description: 'Root Mutuation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a book',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                authorId: { type: GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parent, args) => {
+                return new Promise((resolve, reject) => {
+                    const book = {
+                        name: args.name,
+                        authorId: args.authorId
+                    }
+    
+                    db.run("INSERT INTO book (name, author_id) VALUES (?,?)", [book.name, book.authorId], function(err) {
+                        book.id = this.lastID
+                        resolve(book)
+                    })
+                })
+            }
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an author',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parent, args) => {
+                return new Promise((resolve, reject) => {
+                    const author = {
+                        name: args.name
+                    }
+
+                    db.run("INSERT INTO author (name) VALUES (?)", [author.name], function(err) {
+                        author.id = this.lastID
+                        resolve(author)
+                    })
+                })
+            }
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType,
+    mutation: RootMutationType
+})
+
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}))
+
+app.listen(3000, () => {
+    console.log('Server is running')
+})
